05985e268327969ed985ff6d28ecdd2a765f3565
Alex Cicovic <alex.cicovic@automattic.com>
2026-02-25 07:46:53 +0200

    Eliminate global lookups via dependency injection

--- tests/unit/PluginTest.php
+++ tests/unit/PluginTest.php
@@ -46,29 +46,7 @@ class PluginTest extends TestCase {
 	public function test_init_creates_required_instances(): void {
 		$this->plugin->init();
 
-		$this->assertNotNull( $this->plugin->get_api() );
-		$this->assertNotNull( $this->plugin->get_admin_handler() );
 		$this->assertNotNull( $this->plugin->get_safe_publish_api() );
-	}
-
-	/**
-	 * Verifies that get_api returns an External_Posts_API instance.
-	 */
-	public function test_get_api_returns_external_posts_api(): void {
-		$this->plugin->init();
-
-		$api = $this->plugin->get_api();
-		$this->assertInstanceOf( \Safe_Publish\API\External_Posts_API::class, $api );
-	}
-
-	/**
-	 * Verifies that get_admin_handler returns an Admin_Handler instance.
-	 */
-	public function test_get_admin_handler_returns_admin_handler(): void {
-		$this->plugin->init();
-
-		$admin_handler = $this->plugin->get_admin_handler();
-		$this->assertInstanceOf( \Safe_Publish\Admin\Admin_Handler::class, $admin_handler );
 	}
 
 	/**
@@ -79,13 +57,5 @@ class PluginTest extends TestCase {
 
 		$safe_publish_api = $this->plugin->get_safe_publish_api();
 		$this->assertInstanceOf( \Safe_Publish\API\Safe_Publish_API::class, $safe_publish_api );
-	}
-
-	/**
-	 * Verifies that get_admin_handler returns null before initialization.
-	 */
-	public function test_get_admin_handler_before_init_returns_null(): void {
-		$admin_handler = $this->plugin->get_admin_handler();
-		$this->assertNull( $admin_handler );
 	}
 }
