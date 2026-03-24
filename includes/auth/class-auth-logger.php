2a43f77776e4cd1993b6eeeda9edcda642224324
Alex Cicovic <alex.cicovic@automattic.com>
2026-03-24 16:11:44 +0200

    Remove unused constants

--- includes/auth/class-auth-logger.php
+++ includes/auth/class-auth-logger.php
@@ -15,28 +15,6 @@ use Safe_Publish\Utils\Logger;
 class Auth_Logger extends Logger {
 
 	/**
-	 * Events that represent a successful authentication attempt.
-	 *
-	 * @var string[]
-	 */
-	private const SUCCESS_EVENTS = array(
-		'AUTH_SUCCESS',
-	);
-
-	/**
-	 * Events that represent a failed authentication attempt.
-	 *
-	 * @var string[]
-	 */
-	private const FAILURE_EVENTS = array(
-		'NO_SECRET_CONFIGURED',
-		'TIMESTAMP_EXPIRED',
-		'CONTENT_HASH_MISSING',
-		'CONTENT_HASH_MISMATCH',
-		'SIGNATURE_INVALID',
-	);
-
-	/**
 	 * Constructs the Auth_Logger instance.
 	 */
 	public function __construct() {
